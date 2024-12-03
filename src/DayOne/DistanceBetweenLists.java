28968e2d1447b4d2f5b3551e977d2c714e615f08
Grace Jung <gjung20@my.bcit.ca>
2024-12-02 22:30:48 -0800

    finished part 2 of day 1

--- src/DayOne/DistanceBetweenLists.java
+++ src/DayOne/DistanceBetweenLists.java
@@ -1,6 +1,7 @@
 package DayOne;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,6 +23,8 @@ public class DistanceBetweenLists {
         ArrayList<Integer> rightList = new ArrayList<>();
         
         int totalDistance = 0;
+        int occurrences = 0;
+        int similarityScore = 0;
         
         //scans the copied lists
         Scanner filescan = new Scanner(new File("Day1Part1List.txt"));
@@ -45,7 +48,13 @@ public class DistanceBetweenLists {
 //        System.out.println(rightList);
         
         System.out.println("The total distance between the two lists is: " + totalDistance);
-
+        
+        for (int num : leftList) {
+            occurrences = Collections.frequency(rightList, num);
+            similarityScore += num * occurrences;
+        }
+        
+        System.out.println("Their similarity score is: " + similarityScore);
     }
     
 }
